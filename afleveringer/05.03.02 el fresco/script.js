b9c371d98e112b7288822f0b1ff0c8a17e5b743a
Irene Seedorff <iren3024@stud.kea.dk>
2019-02-21 15:25:58 +0100

    burgermenu virker halvt

---
+++ afleveringer/05.03.02 el fresco/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", sidenVises);
+
+
+function sidenVises() {
+    console.log("siden vises");
+    // registrer klik på menuknap
+    document.querySelector(".menu-button").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("Toggle menu");
+    document.querySelector("#menu").classList.toggle("hide");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hide")
+
+    if (erSkjult == true) {
+        //menuen er skjult - ændr menuknap til lll
+        document.querySelector(".menu-button img").src = "materiale/burger.png";
+    } else {
+        //menuen er nu vist - ændr menuknap til X
+        document.querySelector(".menu-button img").src = "materiale/cancel.png";
+    }
+}
+
+//BURGERMENU SLUT
