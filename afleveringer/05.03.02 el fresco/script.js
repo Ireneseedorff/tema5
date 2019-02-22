c6e8bba1ec2f9ca7d6911c33d0897dbdb5e3f26e
Irene Seedorff <iren3024@stud.kea.dk>
2019-02-22 09:02:18 +0100

    menu føste virker

--- afleveringer/05.03.02 el fresco/script.js
+++ afleveringer/05.03.02 el fresco/script.js
@@ -23,3 +23,71 @@ function toggleMenu() {
 }
 
 //BURGERMENU SLUT
+
+//MENU- OG FILTRERINGSSEKTION
+        let alleRetter = [];
+        let filter = "alle";
+
+
+        document.addEventListener("DOMContentLoaded", start);
+
+
+        function start() {
+            let dest = document.querySelector("#liste");
+
+            async function getJson() {
+                let jsonData = await fetch("https://mandalskeawebspace.dk/claude_php/clean_up_spreadsheet.php?id=1l94dBfaWqqmgz6Dwoxwh3YBV9PEFDsUnnh1A6TPIzbw");
+                alleRetter = await jsonData.json();
+               
+                visRetter();
+            }
+
+            function visRetter() {
+                dest.innerHTML = "";
+                alleRetter.forEach(ret => {
+                    console.log(ret.titel)
+                    if (filter == "alle" || filter == ret.kategori) {
+
+                        let template = `
+                            <article class="ret">
+                            <img src="elfresco_mad/${ret.billede}.jpg" alt="${ret.titel}">
+                            <h2>${ret.titel}</h2>
+                            </article>
+                            `;
+                        dest.insertAdjacentHTML("beforeend", template);
+                        dest.lastElementChild.addEventListener("click", åbn);
+
+                        function åbn() {
+                            document.querySelector("#indhold").innerHTML = `
+                         <article class="ret">
+                    <img src="elfresco_mad/${ret.billede}.jpg" alt="${ret.titel}">
+                    <h2>${ret.titel}</h2>
+            <p>${ret.kortbeskrivelse}</p>
+                <p>Pris: ${ret.pris},-</p>
+                    </article>
+                    `;
+                            document.querySelector("#popup").style.display = "block";
+                        }
+                    }
+
+                })
+            } //function visPersoner slut
+            document.querySelector("#luk button").addEventListener("click", () => {
+                document.querySelector("#popup").style.display = "none";
+            })
+            document.querySelectorAll(".filter").forEach(elm => {
+                elm.addEventListener("click", filtrering);
+            })
+
+            function filtrering() {
+                filter = this.getAttribute("data-kategori");
+                document.querySelector("h2").textContent = this.textContent;
+                document.querySelectorAll(".filter").forEach(elm => {
+                    elm.classList.remove("valgt");
+                })
+                this.classList.add("valgt");
+                visRetter();
+            }
+
+            getJson();
+        }
